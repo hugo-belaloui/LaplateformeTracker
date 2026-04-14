5f0c037895cb70b9e14fc22f9a2841a7ac5e9fb1
achraf_nait_belkacem <achrafoonb@gmail.com>
2026-04-14 13:53:05 +0200

    feature/backend-model: finishing all three models (user,student,teacher)

--- src/main/java/Model/Student.java
+++ src/main/java/Model/Student.java
@@ -1,5 +1,42 @@
 package Model;
+import utils.databaseconnection;
+import java.sql.*;;
 
 public class Student {
-    
+    private Long id ;
+    private String name;
+    private Long userId;
+
+    public Student (Long id, String name, Long userId){
+        this .id = id;
+        this.name = name;
+        this.userId = userId;
+    }
+
+    public Long getId(){return id;}
+    public String getName(){return name;}
+    public Long getUserId(){return userId;}
+
+    public static Student findByUserId(Long userId){
+        try (Connection conn = databaseconnection.getconnection() ){
+            String sql = "SELECT FROM students WHERE user_id = ?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setLong(1, userId);
+
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                return new Student(
+                    rs.getLong("id"),
+                    rs.getString("name"),
+                    rs.getLong("user_id")
+                );
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
 }
