c05d7b056c55a242e60832570632214b47ec705b
hugo-belaloui <hugo.belaloui@laplateforme.io>
2026-04-18 16:54:56 +0200

    feat(database): implement DatabaseConnector and configuration template
       - Add DatabaseConnector utility to manage JDBC connections using a
         properties file.
       - Implement static block for one-time loading of database credentials from
         the classpath.
       - Provide database.properties.template to guide local environment
         configuration.
       - Update .gitignore to ensure sensitive database credentials in
         database.properties are not tracked.

---
+++ Laplateforme_Tracker/src/main/java/com/plateforme_tracker/database/DatabaseConnector.java
@@ -0,0 +1,60 @@
+package com.plateforme_tracker.database;
+
+import java.io.IOException; //deals with input errors
+import java.io.InputStream; 
+import java.sql.Connection; //db connector
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties; // stores key=value from properties
+
+public class DatabaseConnector {
+
+    private static final Properties properties = new Properties();
+
+
+    static   //only execute once when launch program and load class
+
+    {
+        //classpath java looks for database.properties without absolute path
+        try (InputStream input = DatabaseConnector.class.getClassLoader().getResourceAsStream("database.properties"))   
+        {
+            if (input == null) 
+            {
+            System.out.println("Properties file not found"); //if no file print 
+            }
+            else
+            {
+                properties.load(input);
+            }
+        }
+        catch (IOException excep) //print error for debugging
+        {
+            excep.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() //use the import seen above
+    {
+        Connection connection = null;
+        try 
+        {
+            //retrieve data from properties file
+            String url = properties.getProperty("db.url");
+            String user = properties.getProperty("db.user");
+            String password = properties.getProperty("db.password");
+
+            //connection to db using said data
+            connection = DriverManager.getConnection(url, user, password);
+            System.out.print("connected");
+        }
+        catch (SQLException except)
+            {
+                System.out.println("connection failed");
+                except.printStackTrace();
+            }
+        return connection; //object is either connected or return failure
+
+    }
+}
+
+
